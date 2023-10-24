4d87c7468547a0ca0f5a0fff56d7baaab1bf2549
GarrettHiles <146273814+GarrettHiles@users.noreply.github.com>
2023-10-24 17:05:22 -0500

    basic javascript

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+
+var A = "this is a alert with a varaiable";
+
+window.alert(A)
+
+
+// adding ' "" 
+document.write("Hello, \" knock it off bart! or ill tell dad!\" This is Joe\'s mom.");
+//connecting strings
+document.write(" \"I do what I do because I like to do " + "because I am forced to do as well.\"");
+
+var B = " Concatenated" + " String";
+document.write(B);
+//Adding a family varible
+var Family = "The Hiles", Dad = " Eric", Mom = " Tracy", Son1 = " Dylan", Son2 = " Garrett";
+
+document.write(Son2);
+//adding a expression
+document.write( 5 + 5);
+
+var Sent1=" This is the beginning of the string";
+
+var Sent2=" and this is the end of the string";
+
+document.write(Sent1 + Sent2)
