f10ee6803e524023cb1ece8f69bcc99ee5836fa1
GarrettHiles <146273814+GarrettHiles@users.noreply.github.com>
2023-11-06 15:55:39 -0600

    Added Project 9

---
+++ Basic-JavaScript-Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,46 @@
+// This function sets a timer and alerts the browser when the timer is up. 
+
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000); //The 1000 stands for milliseconds.
+        if (seconds == -1) {
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+//shows one image at a time
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+// changing images function
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; // This say its an error in the chrome dev tools but it doesnt effect the slide show.
+  dots[slideIndex-1].className += " active";
+}
