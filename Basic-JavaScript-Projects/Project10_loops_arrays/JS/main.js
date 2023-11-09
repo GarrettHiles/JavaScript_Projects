86e764d16169f4f9cc3fa5ce4fb55b93c550c8fc
GarrettHiles <146273814+GarrettHiles@users.noreply.github.com>
2023-11-09 16:34:55 -0600

    Added comments throughout code.

--- Basic-JavaScript-Projects/Project10_loops_arrays/JS/main.js
+++ Basic-JavaScript-Projects/Project10_loops_arrays/JS/main.js
@@ -1,3 +1,5 @@
+
+// 
 function count_To_Ten() {
     var Digit = "";
     var X = 1;
@@ -7,7 +9,7 @@ function count_To_Ten() {
     }
     document.getElementById("Counting_to_Ten").innerHTML = Digit;
 }
-
+//list of instruments 
 var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
 var Content = "";
 var Y;
@@ -18,7 +20,7 @@ function for_Loop() {
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
 
-
+// function with different cat photos and it displays catpicture 2 with some string behind it.
 function cat_pics() {
     var Cat_Picture =[];
     Cat_Picture[0] = "sleeping";
@@ -28,14 +30,14 @@ function cat_pics() {
     document.getElementById("Cat").innerHTML = "in this picture, the cat is " + Cat_Picture[2] + ".";
 
 }
-
+//Introduces a guitar object and changes the color and adds a price.
 function constant_function() {
     const Musical_Instument = {type:"guitar", brand:"Fender", color:"black"};
     Musical_Instument.color = "blue";
     Musical_Instument.price = "$900";
     document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instument.type + " was " + Musical_Instument.price;
 }
-
+//This was just to use a let.
 var G = 72;
 document.write(G);
 {
@@ -50,7 +52,7 @@ function add(a, b) {
 const result = add(2, 3);
 console.log(result);
 
-//This object is a game
+//This object is a game with use of const instead of a let.
 const game = {
     type: "RPG",
     platform: "PS5",
@@ -62,14 +64,14 @@ const game = {
     }
 }
 document.getElementById("Game_object").innerHTML = game.description();
-
+//a break statement that stops after 2.
 let text = "";
 for (let g = 0; g < 10; g++) {
     if (g===2) {break;}
     text += "The number is " + g + "<br>";
 }
 document.getElementById("break").innerHTML = text;
-
+// continue statement that counts up to 10 and skips 3.
 let write = " ";
 for (let i = 0; i < 10; i++) {
     if (i===3) { continue; }
