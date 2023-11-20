4b73b3771a28bd110d911137826a039f50f1fc81
GarrettHiles <146273814+GarrettHiles@users.noreply.github.com>
2023-11-20 15:59:53 -0600

    Added a lightbox for the photos.

---
+++ One-Page Website/javaScript/One-Page-Website.js
@@ -0,0 +1,37 @@
+//open the model
+function openModel() {
+    document.getElementById("myModel").style.display = "block";
+}
+
+function closeModel() {
+    document.getElementById("myModel").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("control");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < dots.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+}
